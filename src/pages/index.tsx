ae4276e65f134469ede6a50f471ba1a5f3ac951f
szuluaga-beep <steven000069@hotmail.com>
2023-10-19 18:59:12 -0500

    Creando estructura de directorios

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { Typography } from '@mui/material'
+import { ShopLayout } from '../../components/layout'
 
 const index = () => {
   return (
-    <Typography variant='h1'>
-      Teslo Shop
-    </Typography>
+    <ShopLayout title='Shop' pageDescription='Encuentra los mejores productos de Teslo Aqio'>
+      <Typography variant="h1" component='h1'>Tienda</Typography>
+      <Typography variant="h2" sx={{ mb: 1 }}>
+        Todos los productos
+      </Typography>
+    </ShopLayout>
   )
 }
 
