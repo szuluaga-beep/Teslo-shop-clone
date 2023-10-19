088aaceacc4f856aafb1a007a113b43b3ee35177
szuluaga-beep <steven000069@hotmail.com>
2023-10-19 18:39:04 -0500

    Estructuando el proyecto

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,22 @@
 import '@/styles/globals.css'
+import { CssBaseline, ThemeProvider } from '@mui/material'
 import type { AppProps } from 'next/app'
+import { Roboto } from 'next/font/google'
+import { lightTheme } from '../../themes'
+
+
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={lightTheme}>
+      <main className={roboto.className}>
+        <CssBaseline />
+        <Component {...pageProps} />
+      </main>
+    </ThemeProvider>
+  )
 }
