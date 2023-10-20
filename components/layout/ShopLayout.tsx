81884446f6e25213467811e38a118a46d29ab764
szuluaga-beep <steven000069@hotmail.com>
2023-10-19 20:06:02 -0500

    SideMenu

--- components/layout/ShopLayout.tsx
+++ components/layout/ShopLayout.tsx
@@ -1,6 +1,6 @@
 import { FC, ReactNode } from 'react'
 import Head from "next/head"
-import { Navbar } from '../ui';
+import { Navbar, SideMenu } from '../ui';
 
 interface Props {
     children: ReactNode;
@@ -29,7 +29,7 @@ export const ShopLayout: FC<Props> = ({ children, title, pageDescription, imageF
             <nav>
                 <Navbar />
             </nav>
-            {/* TODO:Sidebar */}
+            <SideMenu/>
             <main style={{
                 margin: '80px auto',
                 maxWidth: '1440px',
