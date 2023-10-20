9901b1bc63a9ddbf0c0a7dbf0d447d03fc1279c7
szuluaga-beep <steven000069@hotmail.com>
2023-10-20 15:18:13 -0500

    Página de carrito de compras

---
+++ src/pages/cart/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import { ShopLayout } from '../../../components/layout'
+import { Box, Button, Card, CardContent, Divider, Grid, Typography } from '@mui/material'
+import { CartList } from '../../../components/cart'
+
+const CartPage = () => {
+    return (
+        <ShopLayout title='Carrito' pageDescription='En esta página encontrás los artículos del carrito'>
+            <Typography variant='h1' component='h1'>Carrito</Typography>
+            <Grid container>
+                <Grid xs={12} sm={7}>
+                    <CartList/>
+                </Grid>
+                <Grid xs={12} sm={5}>
+                    <Card className='summary-card'>
+                        <CardContent>
+                            <Typography variant='h2'>Orden</Typography>
+                            <Divider sx={{ my: 2 }} />
+                            {/* OdenSummary */}
+                            <Box sx={{ mt: 3 }}>
+                                <Button color='secondary' className='circular-btn' fullWidth>Checkout</Button>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                </Grid>
+            </Grid>
+        </ShopLayout>
+    )
+}
+
+export default CartPage
