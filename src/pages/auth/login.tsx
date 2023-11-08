36e0d41ad585479908ca3196f5d4c0b6f070d672
szuluaga-beep <steven000069@hotmail.com>
2023-11-08 06:38:11 -0500

    Formulario de login con react hook form

--- src/pages/auth/login.tsx
+++ src/pages/auth/login.tsx
@@ -2,45 +2,64 @@ import React from 'react'
 import { AuthLayout } from '../../../components/layout'
 import { Box, Button, Grid, TextField, Typography, Link as LinkMaterial } from '@mui/material'
 import Link from 'next/link'
+import { useForm } from 'react-hook-form'
+
+interface FormData {
+    email: string;
+    password: string;
+}
+
 
 const LoginPage = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm<FormData>()
+
+    const onLoginUser = (data: FormData) => {
+        // console.log({data})
+    }
     return (
         <AuthLayout title='Ingresar'>
-            <Box sx={{ width: '350px', padding: '10px 20px' }}>
-                <Grid container spacing={4}>
-                    <Grid item xs={12}>
-                        <Typography variant='h1' component={'h1'}>Iniciar sesión</Typography>
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            label="Correo"
-                            type='email'
-                            variant='filled'
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <TextField
-                            fullWidth
-                            label="contraseña"
-                            type='password'
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button color='secondary' className='circular-btn' size='large' fullWidth>
-                            Ingresar
-                        </Button>
-                    </Grid>
-                    <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'end' }}>
-                        <Link href='/auth/register' passHref legacyBehavior>
-                            <LinkMaterial underline='always'>
-                                ¿No tienes cuenta?
-                            </LinkMaterial>
-                        </Link>
-                    </Grid>
-                </Grid>
-            </Box>
+            <form onSubmit={handleSubmit(onLoginUser)}>
 
+                <Box sx={{ width: '350px', padding: '10px 20px' }}>
+                    <Grid container spacing={4}>
+                        <Grid item xs={12}>
+                            <Typography variant='h1' component={'h1'}>Iniciar sesión</Typography>
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                label="Correo"
+                                type='email'
+                                variant='filled'
+                                {...register("email")}
+
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <TextField
+                                fullWidth
+                                label="contraseña"
+                                type='password'
+                                {...register("password")}
+                            />
+                        </Grid>
+                        <Grid item xs={12}>
+                            <Button type='submit' color='secondary' className='circular-btn' size='large' fullWidth>
+                                Ingresar
+                            </Button>
+                        </Grid>
+                        <Grid item xs={12} sx={{ display: 'flex', justifyContent: 'end' }}>
+                            <Link href='/auth/register' passHref legacyBehavior>
+                                <LinkMaterial underline='always'>
+                                    ¿No tienes cuenta?
+                                </LinkMaterial>
+                            </Link>
+                        </Grid>
+                    </Grid>
+                </Box>
+
+            </form>
         </AuthLayout>
     )
 }
