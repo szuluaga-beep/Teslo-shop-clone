acf72550dd7210b8d7d8cbcb37f1bf6a0183459a
szuluaga-beep <steven000069@hotmail.com>
2023-11-08 15:11:53 -0500

    validando token

--- context/auth/AuthProvider.tsx
+++ context/auth/AuthProvider.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode, useReducer } from 'react';
+import { FC, ReactNode, useEffect, useReducer } from 'react';
 import Cookies from "js-cookie";
 import axios from "axios";
 import { AuthContext, authReducer } from './';
@@ -26,6 +26,29 @@ export const AuthProvider: FC<PropsProvider> = ({ children }) => {
 
     const [state, dispatch] = useReducer(authReducer, AUTH_INITIAL_STATE);
 
+    useEffect(() => {
+        checkToken()
+    }, [])
+
+    const checkToken = async () => {
+
+        if (!Cookies.get("token")) {
+            return
+        }
+        
+        try {
+
+            const { data } = await tesloApi.get('/user/validate-token')
+
+            const { token, user } = data
+            Cookies.set("token", token)
+            dispatch({ type: '[Auth] - Login', payload: user })
+        } catch (error) {
+            Cookies.remove("token")
+        }
+    }
+
+
     const loginUser = async (email: string, password: string): Promise<boolean> => {
 
         try {
@@ -42,18 +65,18 @@ export const AuthProvider: FC<PropsProvider> = ({ children }) => {
         }
     }
 
-    const registerUser = async( name: string, email: string, password: string ): Promise<{hasError: boolean; message?: string}> => {
+    const registerUser = async (name: string, email: string, password: string): Promise<{ hasError: boolean; message?: string }> => {
         try {
             const { data } = await tesloApi.post('/user/register', { name, email, password });
             const { token, user } = data;
-            Cookies.set('token', token );
+            Cookies.set('token', token);
             dispatch({ type: '[Auth] - Login', payload: user });
             return {
                 hasError: false
             }
 
         } catch (error) {
-            if ( axios.isAxiosError(error) ) {
+            if (axios.isAxiosError(error)) {
                 return {
                     hasError: true,
                     message: error.response?.data.message
