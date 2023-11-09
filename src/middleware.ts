51d952fe13cc4f05e3712b7d79432c0be385cd4e
szuluaga-beep <steven000069@hotmail.com>
2023-11-09 17:23:02 -0500

    Next 13 - Middleware con Next Auth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,33 +1,35 @@
 import { NextResponse } from 'next/server'
 import type { NextFetchEvent, NextRequest } from 'next/server'
 import { jwt } from '../utils'
+import { getToken } from 'next-auth/jwt'
 
-export async function middleware(request: NextRequest, ev: NextFetchEvent) {
+export async function middleware(req: NextRequest, ev: NextFetchEvent) {
 
-  const token = request.cookies.get("token")?.value || ''
-  console.log(token)
-  // console.log(res)
-  try {
-    await jwt.isValidToken(token)
-    return NextResponse.next();
-  } catch (error) {
-    // console.log(error)
-    let url = request.nextUrl
-    url.pathname = '/auth/login'
-    return NextResponse.rewrite(url);
+  const session = await getToken({ req, secret: process.env.NEXTAUTH_SECRET })
 
+  // console.log(session)
+  if (!session) {
+    const requestedPage = req.nextUrl.pathname;
+    const url = req.nextUrl.clone();
+    url.pathname = `/auth/login`;
+    url.search = `p=${requestedPage}`;
+    return NextResponse.redirect(url);
   }
+
+  return NextResponse.next()
+
+  // const token = request.cookies.get("token")?.value || ''
+  // console.log(token)
+  // // console.log(res)
   // try {
-
-
+    // await jwt.isValidToken(token)
+  //   return NextResponse.next();
   // } catch (error) {
-  //   console.log(error)
+  //   // console.log(error)
   //   let url = request.nextUrl
-  //   url.pathname= '/auth/login'
-  //   // console.log(url)
-  //   // return Response.redirect('/auth/login');
-  //   // const requestedPage = request.url;
+  //   url.pathname = '/auth/login'
   //   return NextResponse.rewrite(url);
+
   // }
 
 }
